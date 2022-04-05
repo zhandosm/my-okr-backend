103787d7e5c55fd8a5046bf520bf1fe9a22827d9
zhandosm <zhanmdd@gmail.com>
2022-04-06 00:14:23 +0900

    Add jwt token extractor

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,15 +1,28 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
+import { Request } from 'express';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        JwtStrategy.extractJWT,
+        ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ]),
       ignoreExpiration: false,
       secretOrKey: process.env.JWT_SECRET,
     });
+  }
+
+  private static extractJWT(req: Request): string | null {
+    if (req.headers && req.headers.cookie) {
+      let token: string = decodeURI(req.headers.cookie);
+      token = token.replace('Authorization=Bearer', '').trim();
+      return token;
+    }
+    return null;
   }
 
   async validate(payload: any) {
