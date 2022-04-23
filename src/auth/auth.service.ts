e1e4331459191a89f31eccb2054534a81faa91b9
zhandosm <zhanmdd@gmail.com>
2022-04-23 18:07:32 +0900

    Add try/catch handler to auth, dashboard module methods

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { Response } from 'express';
 import { UsersService } from 'src/users/users.service';
 import * as bcrypt from 'bcrypt';
@@ -13,25 +13,35 @@ export class AuthService {
   ) {}
 
   async validateUser(username: string, password: string): Promise<any> {
-    const user = await this.usersService.findOne(username);
-    const match = await bcrypt.compare(password, user.password);
-    if (match) return user;
-    return null;
+    try {
+      const user = await this.usersService.findOne(username);
+      const match = await bcrypt.compare(password, user.password);
+      if (match) return user;
+      return null;
+    } catch (err) {
+      console.log(err);
+      throw new InternalServerErrorException(err.message);
+    }
   }
 
   async login(user: any, res: Response) {
-    const payload = { username: user.username, sub: user._id };
-    const accessToken = this.jwtService.sign(payload);
-    res.setHeader(
-      'Set-Cookie',
-      serialize('Authorization', `Bearer ${accessToken}`, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV !== 'development',
-        sameSite: 'none',
-        maxAge: 30 * 24 * 60 * 60,
-        path: '/',
-      }),
-    );
-    return res.status(201).json({ access_token: accessToken });
+    try {
+      const payload = { username: user.username, sub: user._id };
+      const accessToken = this.jwtService.sign(payload);
+      res.setHeader(
+        'Set-Cookie',
+        serialize('Authorization', `Bearer ${accessToken}`, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV !== 'development',
+          sameSite: 'none',
+          maxAge: 30 * 24 * 60 * 60,
+          path: '/',
+        }),
+      );
+      return res.status(201).json({ access_token: accessToken });
+    } catch (err) {
+      console.log(err);
+      throw new InternalServerErrorException(err.message);
+    }
   }
 }
--- src/dashboard/dashboard.service.ts
+++ src/dashboard/dashboard.service.ts
@@ -1,4 +1,8 @@
-import { ConsoleLogger, Injectable } from '@nestjs/common';
+import {
+  ConsoleLogger,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { ProjectsService } from '../projects/projects.service';
 import { ObjectivesService } from '../objectives/objectives.service';
@@ -16,41 +20,46 @@ export class DashboardService {
   ) {}
 
   async initialData(userId: string): Promise<any> {
-    interface InitialData {
-      user: { _id: Types.ObjectId; username: string };
-      projects: { _id: Types.ObjectId; title: string; isPinned: boolean }[];
-      initialProject: {
-        _id: Types.ObjectId;
-        title: string;
-        objectives: Objective[];
-      };
-    }
-    const userData: any = await this.usersService.findById(userId);
-    const projects: Project[] = await this.projectsService.find(userId);
-    let initialProject: Project = projects[0];
-    for (const project of projects) {
-      if (project.isPinned) initialProject = project;
-    }
-    const objectives = await this.objectivesService.find(
-      userId,
-      initialProject._id,
-      'project',
-    );
-    const data: InitialData = {
-      user: { _id: userData._id, username: userData.username },
-      projects: projects.map((project) => {
-        return {
-          _id: project._id,
-          title: project.title,
-          isPinned: project.isPinned,
+    try {
+      interface InitialData {
+        user: { _id: Types.ObjectId; username: string };
+        projects: { _id: Types.ObjectId; title: string; isPinned: boolean }[];
+        initialProject: {
+          _id: Types.ObjectId;
+          title: string;
+          objectives: Objective[];
         };
-      }),
-      initialProject: {
-        _id: initialProject._id,
-        title: initialProject.title,
-        objectives: objectives,
-      },
-    };
-    return data;
+      }
+      const userData: any = await this.usersService.findById(userId);
+      const projects: Project[] = await this.projectsService.find(userId);
+      let initialProject: Project = projects[0];
+      for (const project of projects) {
+        if (project.isPinned) initialProject = project;
+      }
+      const objectives = await this.objectivesService.find(
+        userId,
+        initialProject._id,
+        'project',
+      );
+      const data: InitialData = {
+        user: { _id: userData._id, username: userData.username },
+        projects: projects.map((project) => {
+          return {
+            _id: project._id,
+            title: project.title,
+            isPinned: project.isPinned,
+          };
+        }),
+        initialProject: {
+          _id: initialProject._id,
+          title: initialProject.title,
+          objectives: objectives,
+        },
+      };
+      return data;
+    } catch (err) {
+      console.log(err);
+      throw new InternalServerErrorException(err.message);
+    }
   }
 }
